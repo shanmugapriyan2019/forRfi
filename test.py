a94fa61ba0a8bc9f5b65f8d6547d6fe22f7a8043
shanmugapriyan2019 <56590223+shanmugapriyan2019@users.noreply.github.com>
2022-08-05 07:22:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,2 @@
 print("Github RFI exploited")
-import requests
-import pdb
 
-res = requests.get("https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=AAPL&fields=regularMarketPrice")
-# If you need to work around SSL issues, set the verify kw arg to False. For example:
-# requests.get("URL_HERE", verify=False)
-
-stock_data = res.json()
-price = stock_data['quoteResponse']['result'][0]['regularMarketPrice']
-print(price)
